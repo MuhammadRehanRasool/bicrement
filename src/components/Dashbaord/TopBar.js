ca72d53ed05bc593bc8892243e688f6cceda1faa
abar ahmed <abrarahmed2185@gmail.com>
2022-08-12 20:55:12 +0500

    pull

--- src/components/Dashbaord/TopBar.js
+++ src/components/Dashbaord/TopBar.js
@@ -19,7 +19,7 @@ const TopBar = () => {
                     </p>
 
                     <div className="text-center">
-                        <h1 className="text-lg">Welcome! Ibu Hatela</h1>
+                        <h1 className="text-lg">Welcome! Ibu Hatela</h1>    
                         <p className="text-xs">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Accusantium, magni.
