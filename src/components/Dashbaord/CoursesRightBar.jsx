978003b41ebbf8425b453e0cbccb493ddd487a11
abar ahmed <abrarahmed2185@gmail.com>
2022-08-13 09:47:47 +0500

    Class  live UI

--- src/components/Dashbaord/CoursesRightBar.jsx
+++ src/components/Dashbaord/CoursesRightBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Notification from "./Notification";
 import TimeTable from "./TimeTable";
-
+import playBtn from  '../../Assets/icons/video.png'
 const CoursesRightBar = () => {
     return (
         <div className="w-[30rem] space-y-5">
@@ -9,6 +9,12 @@ const CoursesRightBar = () => {
            <div className="h-48">
            <TimeTable/>
            </div>
+            <div className="w-40 rounded-3xl shadow-md flex flex-col justify-between items-center p-8 space-y-2 relative">
+            <p className="absolute right-0 w-5 h-5 bg-red-500 rounded-full top-0">.</p>
+                <p className="text-grays-light text-sm">Class is live !</p>
+                <img src={playBtn} alt="" />
+                <p className="text-content text-md">Enter</p>
+            </div>
         </div>
     );
 };
