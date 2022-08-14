4901ccc71acd84cd3dc528ebbade3d602fd3da8a
abar ahmed <abrarahmed2185@gmail.com>
2022-08-14 20:29:30 +0500

    dashboard live class bug

--- src/components/Dashbaord/CoursesRightBar.jsx
+++ src/components/Dashbaord/CoursesRightBar.jsx
@@ -6,12 +6,12 @@ const CoursesRightBar = () => {
     return (
         <div className="w-full md:w-[30rem] space-y-10 mt-10 md:mt-0">
             <Notification />
-           <div className="h-56">
+           <div className="md:h-56">
            <TimeTable/>
            </div>
            
             <div className="md:w-40 rounded-3xl shadow-md flex flex-col justify-between items-center p-8 space-y-2 relative">
-            <p className="absolute right-0 w-5 h-5 bg-red-500 rounded-full top-0">.</p>
+            <p className="absolute right-0 w-5 h-5 bg-red-500 rounded-full top-0"></p>
                 <p className="text-grays-light text-sm">Class is live !</p>
                 <img src={playBtn} alt="" />
                 <p className="text-content text-md">Enter</p>
